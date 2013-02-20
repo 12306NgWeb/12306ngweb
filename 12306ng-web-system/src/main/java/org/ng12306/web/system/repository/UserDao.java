c1a03f81f15451887fa76674f916cfc250aab091
Radium <superxielei@gmail.com>
2013-02-20 21:28:01 +0800

    删除系统管理平台
    将两个工程方在一个仓库中，后续更新会出现混乱的情况。而且暂时没有开发计划，需要开发时将另外启动一个新的仓库来存放。

--- 12306ng-web-system/src/main/java/org/ng12306/web/system/repository/UserDao.java
+++
@@ -1,8 +0,0 @@
-package org.ng12306.web.system.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.ng12306.web.system.entity.User;
-
-public interface UserDao extends PagingAndSortingRepository<User, Long> {
-	User findByLoginName(String loginName);
-}
